331d5a590647386d862b7d3364f36b51408b840c
victorconcetta <victorconcetta@gmail.com>
2026-05-21 13:51:45 -0300

    asdsadsadsadsd

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -22,10 +22,10 @@ function Footer() {
 
    <S.UlMenu>
             <h1>Menu</h1>
-          <S.LiMenu><Link to="/" onClick={() => setSubProdutos(false)}>Home</Link></S.LiMenu>
-          <S.LiMenu><Link to="/ofertas" onClick={() => setSubProdutos(false)}>Ofertas</Link></S.LiMenu>
-          <S.LiMenu><Link to="/unidades" onClick={() => setSubProdutos(false)}>Unidades</Link></S.LiMenu>
-          <S.LiMenu><Link to="/contato" onClick={() => setSubProdutos(false)}>Contato</Link></S.LiMenu>
+ <S.LiMenu><Link to="/">Home</Link></S.LiMenu>
+<S.LiMenu><Link to="/ofertas">Ofertas</Link></S.LiMenu>
+<S.LiMenu><Link to="/unidades">Unidades</Link></S.LiMenu>
+<S.LiMenu><Link to="/contato">Contato</Link></S.LiMenu>
         </S.UlMenu>
 <S.Produtos>
 <h1>Produtos</h1>
